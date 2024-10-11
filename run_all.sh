c0f0dd1d44738c223cf6b8e35fbf3119d3b4aa90
iamnotyourspy <iamnotyourspy.gmail>
2024-10-11 19:08:31 +0800

    update

--- run_all.sh
+++ run_all.sh
@@ -6,4 +6,4 @@ python3 get_statistics_result.py >> ${OUTPUT}
 
 time=$(TZ=Asia/Shanghai date '+%Y-%m-%d %H:%M:%S')
 echo "\n\n上次更新时间(UTC+8): ${time}" >> ${OUTPUT}
-echo "\n\n填写问卷: [问卷](https://forms.gle/bxUKH95Yq54SVNvp8)" >> ${OUTPUT}
+echo "\n\n诚邀您填写问卷: [问卷](https://forms.gle/bxUKH95Yq54SVNvp8)" >> ${OUTPUT}
