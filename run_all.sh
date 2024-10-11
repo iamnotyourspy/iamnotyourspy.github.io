1b2be97dee8f21e92ba2043ccf1576cb5d51b395
iamnotyourspy <iamnotyourspy.gmail>
2024-10-11 17:54:26 +0800

    first commit

---
+++ run_all.sh
@@ -0,0 +1,9 @@
+OUTPUT="./index.md"
+
+echo "# 分类型安调时长统计\n" > ${OUTPUT}
+
+python3 get_statistics_result.py >> ${OUTPUT}
+
+time=$(TZ=Asia/Shanghai date '+%Y-%m-%d %H:%M:%S')
+echo "\n\n上次更新时间(UTC+8): ${time}" >> ${OUTPUT}
+echo "\n\n填写问卷: [问卷](https://forms.gle/bxUKH95Yq54SVNvp8)" >> ${OUTPUT}
